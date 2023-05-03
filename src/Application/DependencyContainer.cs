0227b81844cbf98da82d02cd6d65acc0a88202d2
David Hdez <david.2.hernandez@atos.net>
2023-05-03 17:05:00 -0600

    pub and sub

--- src/Application/DependencyContainer.cs
+++ src/Application/DependencyContainer.cs
@@ -5,18 +5,44 @@ using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using MassTransit;
 
 namespace Application
 {
-    public static class DependencyContainer
-    {
-        public static void AddApplicationLayer(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());
-            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(typeof(CreateClientCommand).Assembly));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+	public static class DependencyContainer
+	{
+		public static void AddApplicationLayer(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddAutoMapper(Assembly.GetExecutingAssembly());
+			services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+			services.AddMediatR(cfg =>
+				cfg.RegisterServicesFromAssembly(typeof(CreateClientCommand).Assembly));
+			services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
 
-        }
-    }
+			services.AddMassTransit(cfg =>
+			{
+				cfg.AddConsumer<PositionCreatedConsumer>();
+				cfg.UsingRabbitMq((ctx, cfgrmq) =>
+				{
+					cfgrmq.Host("amqp://guest:guest@localhost:5672");
+					cfgrmq.ReceiveEndpoint("ClientServiceQueue", econfigureEndpoint =>
+					{
+						econfigureEndpoint.ConfigureConsumeTopology = false;
+						econfigureEndpoint.Durable = true;
+						econfigureEndpoint.UseMessageRetry(retryConfigure =>
+						{
+							retryConfigure.Interval(5, TimeSpan.FromMilliseconds(1000));
+						});
+						econfigureEndpoint.ConfigureConsumer<PositionCreatedConsumer>(ctx);
+
+						econfigureEndpoint.Bind("Atos.Core.EventsDTO:PositionCreated", d =>
+						{
+							d.ExchangeType = "topic";
+							d.RoutingKey = "position.created";
+						});
+					});
+				});
+			});
+		}
+	}
 }
